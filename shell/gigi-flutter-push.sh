046f70c636afc53baab3029024b0abb0619f441d
ison <isonz@users.noreply.github.com>
2022-11-22 14:17:31 +0800

    Create gigi-flutter-push.sh

---
+++ shell/gigi-flutter-push.sh
@@ -0,0 +1,33 @@
+# push.sh
+  
+  #!/bin/bash
+
+# usage: ./pub.sh, ./pub.sh get,  ./pub.sh upgrade , ./pub.sh outdated
+
+# modules=("./")
+for i in $( ls -d ./packages/*/)  #只显示目录
+do
+  modules+=("$i")   # 添加元素到数组后面
+done
+
+loopModules() {
+  op=$1
+  if [ "$op" == "" ] ; then
+    op="get"
+  fi
+
+  for i in "${!modules[@]}";
+  do
+    cd "${modules[$i]}" || continue
+    pwd
+    flutter pub $op
+    cd ../../
+    # echo "${modules[$i]} $op"
+  done
+
+  flutter pub $op
+}
+
+loopModules "$1"
+
+
